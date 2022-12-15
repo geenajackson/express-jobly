051abcdeafad2aa33fda79f21600fbd707c41329
Geena Jackson <geena.jackson96@gmail.com>
2022-12-15 09:46:24 -0600

    document sqlForPartialUpdate helper

--- helpers/sql.js
+++ helpers/sql.js
@@ -1,6 +1,12 @@
 const { BadRequestError } = require("../expressError");
 
-// THIS NEEDS SOME GREAT DOCUMENTATION.
+/** Allows for partial update of an existing value in the database.
+ * 
+ * Converts values from JSON to SQL and maps values into an entry.
+ * 
+ * Will reject empty requests.
+*/
+
 
 function sqlForPartialUpdate(dataToUpdate, jsToSql) {
   const keys = Object.keys(dataToUpdate);
@@ -8,7 +14,7 @@ function sqlForPartialUpdate(dataToUpdate, jsToSql) {
 
   // {firstName: 'Aliya', age: 32} => ['"first_name"=$1', '"age"=$2']
   const cols = keys.map((colName, idx) =>
-      `"${jsToSql[colName] || colName}"=$${idx + 1}`,
+    `"${jsToSql[colName] || colName}"=$${idx + 1}`,
   );
 
   return {
