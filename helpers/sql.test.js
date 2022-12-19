a43836f07b1c60843116dfa79160717eaab17e1a
Geena Jackson <geena.jackson96@gmail.com>
2022-12-19 09:04:40 -0600

    add tests for updating one or more values

--- helpers/sql.test.js
+++ helpers/sql.test.js
@@ -8,6 +8,19 @@ describe("sqlForPartialUpdate", function () {
             { name: "name" },
             { name: "newName" }
         );
-        expect(result).toEqual("hello")
+        expect(result).toEqual({
+            "setCols": "\"newName\"=$1",
+            "values": ["name"]
+        })
+    });
+    test("updates more than one value", async function () {
+        const result = sqlForPartialUpdate(
+            { name: "name", password: "password", email: "email@email.com" },
+            { name: "newName", password: "newPassword", email: "newemail@email.com" }
+        );
+        expect(result).toEqual({
+            "setCols": "\"newName\"=$1, \"newPassword\"=$2, \"newemail@email.com\"=$3",
+            "values": ["name", "password", "email@email.com"]
+        })
     })
 })
