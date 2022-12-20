0ee8d7961fe92d46e192e15a0d690253e360d027
Geena Jackson <geena.jackson96@gmail.com>
2022-12-20 09:18:13 -0600

    import sameUserOrAdmin and add middleware to delete route

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,7 @@
 const jsonschema = require("jsonschema");
 
 const express = require("express");
-const { ensureLoggedIn, isAdmin } = require("../middleware/auth");
+const { ensureLoggedIn, isAdmin, sameUserOrAdmin } = require("../middleware/auth");
 const { BadRequestError } = require("../expressError");
 const User = require("../models/user");
 const { createToken } = require("../helpers/tokens");
@@ -109,7 +109,7 @@ router.patch("/:username", ensureLoggedIn, async function (req, res, next) {
  * Authorization required: login
  **/
 
-router.delete("/:username", ensureLoggedIn, async function (req, res, next) {
+router.delete("/:username", ensureLoggedIn, sameUserOrAdmin, async function (req, res, next) {
   try {
     await User.remove(req.params.username);
     return res.json({ deleted: req.params.username });
