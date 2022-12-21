4c1cc6d8a43dce3499f03b693258d75c2bf59889
Geena Jackson <geena.jackson96@gmail.com>
2022-12-21 09:25:09 -0600

    create routes for jobs, including get/post/patch/delete

---
+++ routes/jobs.js
@@ -0,0 +1,118 @@
+"use strict";
+
+/** Routes for jobs. */
+
+const jsonschema = require("jsonschema");
+const express = require("express");
+
+const { BadRequestError } = require("../expressError");
+const { ensureLoggedIn, isAdmin } = require("../middleware/auth");
+const Job = require("../models/job");
+
+const jobNewSchema = require("../schemas/jobNew.json");
+const jobUpdateSchema = require("../schemas/jobUpdate.json");
+
+const router = new express.Router();
+
+
+/** POST / { job } =>  { job }
+ *
+ * Job should be { title, salary, equity, companyHandle }
+ *
+ * Returns { id, title, salary, equity, companyHandle }
+ *
+ * Authorization required: login, admin
+ */
+
+router.post("/", ensureLoggedIn, isAdmin, async function (req, res, next) {
+    try {
+        const validator = jsonschema.validate(req.body, jobNewSchema);
+        if (!validator.valid) {
+            const errs = validator.errors.map(e => e.stack);
+            throw new BadRequestError(errs);
+        }
+
+        const job = await Job.create(req.body);
+        return res.status(201).json({ job });
+    } catch (err) {
+        return next(err);
+    }
+});
+
+/** GET /  =>
+ *   { jobs: [ { id, title, salary, equity, companyHandle }, ...] }
+ *
+ * Can filter on provided search filters:
+ * 
+ *
+ * Authorization required: none
+ */
+
+router.get("/", async function (req, res, next) {
+    try {
+        const jobs = await Job.findAll(req.query);
+        return res.json({ jobs: jobs });
+    } catch (err) {
+        return next(err);
+    }
+});
+
+/** GET /[handle]  =>  { job }
+ *
+ *  Job is { id, title, salary, equity, companyHandle }
+ *
+ *
+ * Authorization required: none
+ */
+
+router.get("/:handle", async function (req, res, next) {
+    try {
+        const job = await Job.get(req.params.handle);
+        return res.json({ job });
+    } catch (err) {
+        return next(err);
+    }
+});
+
+/** PATCH /[handle] { fld1, fld2, ... } => { job }
+ *
+ * Patches job data.
+ *
+ * fields can be: { title, salary, equity }
+ *
+ * Returns { id, title, salary, equity, companyHandle }
+ *
+ * Authorization required: login, admin
+ */
+
+router.patch("/:handle", ensureLoggedIn, isAdmin, async function (req, res, next) {
+    try {
+        const validator = jsonschema.validate(req.body, jobUpdateSchema);
+        if (!validator.valid) {
+            const errs = validator.errors.map(e => e.stack);
+            throw new BadRequestError(errs);
+        }
+
+        const job = await Job.update(req.params.handle, req.body);
+        return res.json({ job });
+    } catch (err) {
+        return next(err);
+    }
+});
+
+/** DELETE /[handle]  =>  { deleted: handle }
+ *
+ * Authorization: login, admin
+ */
+
+router.delete("/:handle", ensureLoggedIn, isAdmin, async function (req, res, next) {
+    try {
+        await Job.remove(req.params.handle);
+        return res.json({ deleted: req.params.handle });
+    } catch (err) {
+        return next(err);
+    }
+});
+
+
+module.exports = router;
